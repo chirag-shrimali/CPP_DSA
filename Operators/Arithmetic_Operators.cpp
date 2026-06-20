274cbebb4a3ae46168b83aec5f606ca0accfcc78
Chirag Shrimali <chiragshrimali0411@gmail.com>
2026-06-20 23:12:11 +0530

    Arithmetic Operators

---
+++ Operators/Arithmetic_Operators.cpp
@@ -0,0 +1,28 @@
+// There are mainly having 5 types of arithmetic operators...
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    // int x = 15 , y = 3; // First Way to Variable Declaration and Initialization...
+
+    // 2nd way...
+
+    int x = 15;
+
+    int y = 3;
+
+    std :: cout << "Addition : " << x + y << endl; // 18
+
+    std :: cout << "Subtraction : " << x - y << endl; // 12
+
+    std :: cout << "Multiplication : " << x * y << endl; // 45
+
+    std :: cout << "Division : " << x / y << endl; // 5
+
+    std :: cout << "Remainder(Modulus) : " << x % y << endl; // 0
+
+    return 0;
+}
