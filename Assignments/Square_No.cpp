3896fbb358f969571b69ebc4bec85b2b29559cae
Chirag Shrimali <chiragshrimali0411@gmail.com>
2026-06-21 00:22:42 +0530

    Assignments - 3 Square of a Number

---
+++ Assignments/Square_No.cpp
@@ -0,0 +1,22 @@
+// Square of a number...
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int no;
+
+    int sq;
+
+    cout << "\nEnter the Number : ";
+
+    cin >> no;
+
+    sq = no * no;
+
+    cout << "\nThe Square of a Number is : " << sq;
+
+    return 0;
+}
