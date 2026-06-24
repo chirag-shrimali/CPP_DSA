333b5edf701123a7ef3af07abec286894106ed88
Chirag Shrimali <chiragshrimali0411@gmail.com>
2026-06-24 20:47:22 +0530

    Two No Sum

---
+++ Assignments/Two_No_Sum.cpp
@@ -0,0 +1,26 @@
+// Take two numbers as an input and print their sum...
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a , b;
+
+    int sum = 0;
+
+    cout << "\nEnter the No1 : ";
+
+    cin >> a;
+
+    cout << "\nEnter the No2 : ";
+
+    cin >> b;
+
+    sum = a + b;
+
+    cout << "\nThe Sum of " << a << " and " << b << " are : " << sum;
+
+    return 0;
+}
