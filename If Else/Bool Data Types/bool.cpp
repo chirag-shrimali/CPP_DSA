4d51567777d98261cb14326ab83f817698f4f171
Chirag Shrimali <chiragshrimali0411@gmail.com>
2026-06-27 00:54:17 +0530

    Boolean Data Types

---
+++ If Else/Bool Data Types/bool.cpp
@@ -0,0 +1,20 @@
+// Boiler Plate Code in C++ Language...
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{   
+    // true --> 1 and false --> 0
+
+    bool x = true; // in C++ by default when we print the bool data types like true , false value it will be print respectively 1 and 0 as an output...
+
+    bool x1 = false;
+
+    cout << x << endl; // end the line...
+
+    cout << x1;
+
+    return 0;
+}
