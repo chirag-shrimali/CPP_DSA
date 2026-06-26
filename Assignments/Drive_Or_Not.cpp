3cd293f0a8682b72ae9320c9636eaf86812cf753
Chirag Shrimali <chiragshrimali0411@gmail.com>
2026-06-27 01:13:14 +0530

    Tasks Drive Or Not

---
+++ Assignments/Drive_Or_Not.cpp
@@ -0,0 +1,20 @@
+// Take age as an input and tell if person can drive or not...
+
+// Boiler Plate Code..
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int age;
+
+    cout << "\nEnter Your Age : ";
+
+    cin >> age;
+
+    cout << (age >= 18) << endl; // it will be check that whether entering age is 18 or above 18 print (true --> 1) or print(false --> 0)
+
+    return 0;
+}
