3073dd73335e5713c9030ffaff4b933f86732abc
Chirag Shrimali <chiragshrimali0411@gmail.com>
2026-06-28 23:35:26 +0530

    Short Data Types

---
+++ If Else/Data Types/Short_DataTypes.cpp
@@ -0,0 +1,18 @@
+// Bolier Plate Code...
+
+#include <iostream>
+
+#include <climits>
+
+using namespace std;
+
+int main()
+{
+    short x = SHRT_MAX , y = SHRT_MIN;
+
+    // short y = SHRT_MIN;
+
+    cout << x << endl << y;
+
+    return 0;
+}
