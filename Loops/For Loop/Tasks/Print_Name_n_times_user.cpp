11e77b554b15166cce5fba32b4a30359a07d44b2
Chirag Shrimali <chiragshrimali0411@gmail.com>
2026-07-02 01:57:34 +0530

    Prints The Name N Times

---
+++ Loops/For Loop/Tasks/Print_Name_n_times_user.cpp
@@ -0,0 +1,23 @@
+// Takes an input from user and prints name n times...
+
+// Boiler Plate Code...
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int no;
+
+    cout << "\nEnter the Value of the No : ";
+
+    cin >> no;
+
+    for (int i = 1 ; i <= no ; i ++)
+    {
+        cout << "Hello , Chirag!!" << endl;
+    }
+
+    return 0;
+}
