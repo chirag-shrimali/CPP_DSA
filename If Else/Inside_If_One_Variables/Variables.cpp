7cdffd4768d88e3dfcb2aab788b168bdbaa5ac81
Chirag Shrimali <chiragshrimali0411@gmail.com>
2026-07-02 00:19:13 +0530

    Variables inside if conditions statements

--- If Else/Inside_If_One_Variables/Variables.cpp
+++ If Else/Inside_If_One_Variables/Variables.cpp
@@ -1,5 +1,7 @@
 // Boiler Plate Code...
 
+// Any thing except 0 in c++ when we can be write it will be always prints if statements...
+
 #include <iostream>
 
 using namespace std;
@@ -12,9 +14,21 @@ int main()
 
     // else cout << "Java"; // Here x = 0 shows false statements in C++ so if consitions becomes false thus , else parts prints...
 
-    int x = 1; // consider as true bool data types...
+    // int x = 1; // consider as true bool data types...
 
-    if(x) cout << "C++"; // true --> if(1 --> consider as true statements in c++) thus , if statements can be prints...
+    // if(x) cout << "C++"; // true --> if(1 --> consider as true statements in c++) thus , if statements can be prints...
+
+    // else cout << "Java";
+
+    // int x = 45; // anything except 0 consider as true (1) so if statements will be excuted... 
+
+    // float x = 4.55; // true
+
+    // float x = 0.000; // false --> prints Java
+
+    float x = -0.00004; // consider as true except 0
+
+    if(x) cout << "C++";
 
     else cout << "Java";
 
