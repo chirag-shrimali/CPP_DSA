ea2804c73ca680d0cf8721c3278dc84f84d3aa61
Chirag Shrimali <chiragshrimali0411@gmail.com>
2026-07-02 19:33:10 +0530

    Print the numbers from 1 to 100

---
+++ Loops/For Loop/Tasks/Print_No_1_100.cpp
@@ -0,0 +1,21 @@
+/*
+    Print the numbers from 1 to 100...
+*/
+
+// Boiler Plate Code...
+
+#include <iostream> // Header Files...
+
+using namespace std;
+
+int main()
+{
+    for (int i = 1; i <= 100; i++) // i++ // i += 1 // i = i + 1
+    {
+
+        cout << i << "\t";
+    
+    }
+
+    return 0;
+}
