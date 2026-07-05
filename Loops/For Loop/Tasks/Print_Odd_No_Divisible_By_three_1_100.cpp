049c0d2da291d623fd79108b3347b0681e54dd7d
Chirag Shrimali <chiragshrimali0411@gmail.com>
2026-07-05 23:29:35 +0530

    Print the Odd numbers divisible by 3 from 1 to 100

--- Loops/For Loop/Tasks/Print_Odd_No_Divisible_By_three_1_100.cpp
+++ Loops/For Loop/Tasks/Print_Odd_No_Divisible_By_three_1_100.cpp
@@ -16,10 +16,30 @@ int main()
 {
     int i;
 
-    for (i = 1 ; i <= 100 ; i += 2) // i = i + 2
+    // 100 Times Iterations...
+
+    // for (i = 1 ; i <= 100 ; i += 1) // i = i + 1 // i++
+    // {
+
+    //     if(i % 3 == 0 && i % 2 != 0) cout << i << "\t"; // i % 2 != 0 // i % 2 == 1 
+
+    // }
+
+    // 50 Times Iterations...
+
+    // for (i = 1 ; i <= 100 ; i += 2) // i = i + 2
+    // {
+
+    //     if(i % 3 == 0) cout << i << "\t";
+
+    // }
+
+    // 17 Times Iterations...
+
+    for (i = 3 ; i <= 99 ; i += 6) // i = i + 2
     {
 
-        if(i % 3 == 0) cout << i << "\t";
+        cout << i << "\t";
 
     }
 
