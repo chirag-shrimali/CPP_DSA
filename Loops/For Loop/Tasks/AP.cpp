a316838e388356f801f602756e2ad98781168740
Chirag Shrimali <chiragshrimali0411@gmail.com>
2026-07-06 00:18:41 +0530

    AP TAsk - 1

--- Loops/For Loop/Tasks/AP.cpp
+++ Loops/For Loop/Tasks/AP.cpp
@@ -16,9 +16,13 @@ int main()
 
     // an / tn / Tn = a + (no - 1) * d // an = 2 + (no - 1) * 3 // an = 2 + (3 * no - 3) // an = 3 * no - 1
 
-    for (int i = 2 ; i <= (3 * no - 1) ; i += 3) // i = a ; i <= an ; i += d (i = i + d)
+    // for (int i = 2 ; i <= (3 * no - 1) ; i += 3) // i = a ; i <= an ; i += d (i = i + d)
 
-        cout << i << "\t";
+    //     cout << i << "\t";
+
+    for (int i = 1; i <= no; i++)
+
+        cout << (3 * i - 1) << "\t";
 
     return 0;
 }
