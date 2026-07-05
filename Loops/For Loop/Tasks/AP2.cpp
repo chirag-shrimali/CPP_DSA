56c960faa3698fb30f20efa52a353480933329ff
Chirag Shrimali <chiragshrimali0411@gmail.com>
2026-07-06 00:55:17 +0530

    AP Task - 3

---
+++ Loops/For Loop/Tasks/AP2.cpp
@@ -0,0 +1,21 @@
+// Print this series - 99 , 95 , 91 , 87 , ... , upto all terms which are positive...
+
+// Boiler Plate Code...
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    int no;
+
+    cout << "\nEnter the No of terms : ";
+
+    cin >> no;
+
+
+
+    return 0;
+}
