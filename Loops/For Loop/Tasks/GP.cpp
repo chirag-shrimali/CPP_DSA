93ff6ebca2887760994028075d8ff9e35dc067ce
Chirag Shrimali <chiragshrimali0411@gmail.com>
2026-07-06 00:50:37 +0530

    GP Task - 1

---
+++ Loops/For Loop/Tasks/GP.cpp
@@ -0,0 +1,27 @@
+// Display this GP(Geometric Progression) - 1 , 2 , 4 , 8 , ... upto 'n' terms.
+
+// Boiler Plate Code...
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int no;
+
+    cout << "\nEnter the No : ";
+
+    cin >> no;
+
+    int a = 1;
+
+    for (int i = 1; i <= no; i++)
+    {
+        cout << a << "\t";
+
+        a *= 2; // a = a * 2;
+    }
+    
+    return 0;
+}
