ce1029513bf2d61d5e519b70f613ac7826b8cf38
Chirag Shrimali <chiragshrimali0411@gmail.com>
2026-07-06 00:04:40 +0530

    AP TAsk - 1

---
+++ Loops/For Loop/Tasks/AP.cpp
@@ -0,0 +1,22 @@
+// Display this AP - 2 , 5 , 8 , 11 , ... upto terms....
+
+// Boiler Plate Code...
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int no;
+
+    cout << "\nEnter the No : ";
+
+    cin >> no;
+
+    for (int i = 2 ; i <= no ; i += 3)
+
+        cout << i << "\t";
+
+    return 0;
+}
