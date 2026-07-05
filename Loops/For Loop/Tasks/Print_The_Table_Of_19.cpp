66612e6e734eb0b52b5ff48d837d8a37fe9ec3ca
Chirag Shrimali <chiragshrimali0411@gmail.com>
2026-07-05 23:38:19 +0530

    Print the Table Of 19

---
+++ Loops/For Loop/Tasks/Print_The_Table_Of_19.cpp
@@ -0,0 +1,16 @@
+// Print the Table of 19...
+
+// Boiler Plate Code...
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    for(int i = 19 ; i <= 190 ; i += 19)
+
+    cout << i << "\t";
+
+    return 0;
+}
