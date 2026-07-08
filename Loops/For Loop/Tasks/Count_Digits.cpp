b59f65cad48b047d37ac9c1b927ec760e345d04b
Chirag Shrimali <chiragshrimali0411@gmail.com>
2026-07-09 00:11:26 +0530

    Count Of Digits

---
+++ Loops/For Loop/Tasks/Count_Digits.cpp
@@ -0,0 +1,31 @@
+// Write a Program to check the Count digits of a number...
+
+// Boiler Plate Code...
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int no;
+
+    cout << "\nEnter the No : ";
+
+    cin >> no;
+
+    int count = 0;
+
+    while (no > 0)
+    {
+
+        no /= 10; // no = no / 10;
+
+        count++; // count = count + 1 // count += 1
+    
+    }
+
+    cout << "\nTotal Number of Digits are : " << count;
+
+    return 0;
+}
