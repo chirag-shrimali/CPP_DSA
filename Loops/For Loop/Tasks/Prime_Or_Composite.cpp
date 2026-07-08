2c339209ae1f1d7ea4e313d0da1767a676861e0b
Chirag Shrimali <chiragshrimali0411@gmail.com>
2026-07-08 16:16:44 +0530

    Prime Or Composite No

--- Loops/For Loop/Tasks/Prime_Or_Composite.cpp
+++ Loops/For Loop/Tasks/Prime_Or_Composite.cpp
@@ -39,21 +39,41 @@ int main()
 
     // 2nd Way -----------------------------------------------------------------------------
     
-    int count = 0;
+    // int count = 0;
 
-    for (int i = 2 ; i <= (no - 1) ; i++)
+    // for (int i = 2 ; i <= (no - 1) ; i++)
+    // {
+    //     if(no % i == 0)
+    //     {
+    //         count++; // count++ // count = count + 1 // count += 1;
+            
+    //         break;
+    //     }
+    // }
+    
+    // if (count == 1) cout << endl << no << " is a Composite No...";
+    
+    // else cout << endl << no << " is not a Composite No...";
+
+    // 3rd Way --------------------------------------------------------------------------
+
+    bool flag = false; // initially we are consider in the flag variable which data types is bool here stored false means 0 shows number is prime no...
+
+    for (int i = 2 ; i <= (no - 1) ; i++) // i++ // i += 1 // i = i + 1
     {
         if(no % i == 0)
         {
-            count++; // count++ // count = count + 1 // count += 1;
-            
+        
+            flag = true; // this shows that the flag value changed false to true means the given number between 2 to n - 1 is divisible(factors) to another numbers betwwen the 2 to n - 1 thus if any another number we gets we just break here and print if flag value is true then composite number otherwise not a composite number...
+        
             break;
+
         }
-    }
-    
-    if (count == 1) cout << endl << no << " is a Composite No...";
-    
-    else cout << endl << no << " is not a Composite No...";
+    } 
+
+    if (flag == true) cout << endl << no << " is a Composite No..."; // flag == 1
+
+    else cout << endl << no << " is a Prime No...";
 
     return 0;
 }
