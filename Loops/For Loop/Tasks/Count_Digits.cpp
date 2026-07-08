d09182c9c6603c510951051eeb2418f3f1f43e17
Chirag Shrimali <chiragshrimali0411@gmail.com>
2026-07-09 00:21:36 +0530

    Count Of Digits

--- Loops/For Loop/Tasks/Count_Digits.cpp
+++ Loops/For Loop/Tasks/Count_Digits.cpp
@@ -8,6 +8,32 @@ using namespace std;
 
 int main()
 {
+
+    // 1st Way ---------------------------------------------------------------------------------
+
+    // int no;
+
+    // cout << "\nEnter the No : ";
+
+    // cin >> no;
+
+    // int count = 0;
+
+    // if (no == 0) count++;
+
+    // while (no != 0)
+    // {
+
+    //     no /= 10; // no = no / 10
+
+    //     count++; // count++ // count = count + 1 // count += 1
+    
+    // }
+
+    // cout << "\nThe Total Number of Digits are : " << count;
+
+    // 2nd Way ---------------------------------------------------------------------------------
+
     int no;
 
     cout << "\nEnter the No : ";
@@ -18,7 +44,9 @@ int main()
 
     if (no == 0) count++;
 
-    while (no != 0)
+    if (no < 0) no *= -1; // no = no * -1;
+
+    while (no > 0)
     {
 
         no /= 10; // no = no / 10
