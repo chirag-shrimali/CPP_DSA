dfeb113da7b60317e06c268cc10e2bd75952cfef
Chirag Shrimali <chiragshrimali0411@gmail.com>
2026-07-08 16:42:52 +0530

    Do While Loops Basics

---
+++ Loops/Do While Loop/basic.cpp
@@ -0,0 +1,21 @@
+// Boiler Plate Code...
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int i = 1;
+
+    do
+    {
+
+        cout << i << " ";
+
+        i++;
+
+    } while (i <= 5);
+
+    return 0;
+}
