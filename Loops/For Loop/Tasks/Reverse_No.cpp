b8a78e617964efc1cb424bc860abf1fa45aa4d43
Chirag Shrimali <chiragshrimali0411@gmail.com>
2026-07-09 01:08:42 +0530

    Reverse Number

--- Loops/For Loop/Tasks/Reverse_No.cpp
+++ Loops/For Loop/Tasks/Reverse_No.cpp
@@ -9,6 +9,29 @@ using namespace std;
 int main()
 {
 
+    // 1st Way -------------------------------------------------------------
+
+    // int no;
+
+    // cout << "\nEnter the No : ";
+
+    // cin >> no;
+
+    // if (no == 0) cout << 0;
+
+    // while (no != 0)
+    // {
+
+    //     int rem = no % 10;
+
+    //     cout << rem;
+        
+    //     no /= 10;
+
+    // }
+
+    // 2nd Way -------------------------------------------------------------
+
     int no;
 
     cout << "\nEnter the No : ";
@@ -17,16 +40,20 @@ int main()
 
     if (no == 0) cout << 0;
 
+    int rev = 0;
+
     while (no != 0)
     {
 
         int rem = no % 10;
 
-        cout << rem;
+        rev = (rev * 10) + rem;
         
         no /= 10;
 
     }
+
+    cout << rev;
 
     return 0;
 }
