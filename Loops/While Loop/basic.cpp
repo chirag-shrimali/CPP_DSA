05dea5f7ca37b4a8ac90873585b975d0e438bf0a
Chirag Shrimali <chiragshrimali0411@gmail.com>
2026-07-08 16:35:12 +0530

    While Loops Basics

---
+++ Loops/While Loop/basic.cpp
@@ -0,0 +1,38 @@
+/*
+
+While Loop is a Entry Controlled Loops...
+
+    syntax :-
+
+    initialization
+
+    while (condition)
+    {
+        // while loops blocks / body / work
+        
+        updation / update / increament / decreament
+    }
+
+*/
+
+// Boiler Plate Code...
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int i = 1;
+
+    while (i <= 10)
+    {
+
+        cout << i << "\t";
+
+        i++; // i++ // i += 1 // i = i + 1
+
+    }
+
+    return 0;
+}
