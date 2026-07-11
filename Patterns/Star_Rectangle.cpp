5d277dbbcf8b84db0449b6f2aa1c950e4400766a
Chirag Shrimali <chiragshrimali0411@gmail.com>
2026-07-12 00:32:44 +0530

    Star Rectangle

--- Patterns/Star_Rectangle.cpp
+++ Patterns/Star_Rectangle.cpp
@@ -27,9 +27,26 @@ int main()
 
     // 2nd Way ----------------------------------------------------------------
 
-    for (int i = 1 ; i <= 3 ; i++)
+    // for (int i = 1 ; i <= 3 ; i++) // i++ // i += 1 // i = i + 1
+    // {
+    //     cout << "* * * * *" << endl;
+    // }
+
+    // 3rd Way ----------------------------------------------------------------
+
+    int rows , columns;
+
+    cout << "\nEnter the Rows : ";
+
+    cin >> rows;
+
+    cout << "\nEnter the Columns : ";
+
+    cin >> columns;
+
+    for (int i = 1 ; i <= rows ; i++)
     {
-        for(int j = 1 ; j <= 5 ; j++)
+        for(int j = 1 ; j <= columns ; j++)
         {
             cout << "* ";
         }
