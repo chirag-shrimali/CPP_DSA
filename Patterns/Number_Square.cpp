0a5b3cc56bba121fb72214c0c288f6600a86f5c4
Chirag Shrimali <chiragshrimali0411@gmail.com>
2026-07-12 00:48:11 +0530

    Number Square Pattern

---
+++ Patterns/Number_Square.cpp
@@ -0,0 +1,50 @@
+/*
+
+    Print the Following Star Rectangle Patterns...
+
+    1 2 3 4
+    1 2 3 4
+    1 2 3 4
+    1 2 3 4
+
+*/
+
+// Boiler Plate Code...
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    // 1st Way -------------------------------------------------------------------
+
+    // cout << "1 2 3 4" << endl;
+
+    // cout << "1 2 3 4" << endl;
+
+    // cout << "1 2 3 4" << endl;
+
+    // cout << "1 2 3 4" << endl;
+
+    // 2nd Way -------------------------------------------------------------------
+
+    // for (int i = 1 ; i <= 4 ; i++) // i++ // i += 1 // i = i + 1
+    // {
+    //     cout << "1 2 3 4" << endl;
+    // } 
+
+    // 3rd Way -------------------------------------------------------------------
+
+    for (int i = 1 ; i <= 4 ; i++) // i++ // i += 1 // i = i + 1
+    {
+        for (int j = 1 ; j <= 4 ; j++)
+        {
+            cout << j << " ";
+        }
+            cout << endl;
+    }
+
+
+    return 0;
+}
