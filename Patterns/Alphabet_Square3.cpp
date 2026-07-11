a7365a8d98bb8686bae1cd05ae685120f9c17a4f
Chirag Shrimali <chiragshrimali0411@gmail.com>
2026-07-12 01:34:05 +0530

    Alphabet Square Pattern - 3

---
+++ Patterns/Alphabet_Square3.cpp
@@ -0,0 +1,62 @@
+/*
+
+    Print the Following Star Rectangle Patterns...
+
+    A A A A
+    B B B B
+    C C C C
+    D D D D
+
+*/
+
+// Boiler Plate Code...
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+
+    cout << "\nEnter the Value of N : ";
+
+    cin >> n;
+
+    // 1st Way ---------------------------------------------------------
+
+    // for(int i = 1 ; i <= n ; i++)
+    // {
+    //     for(int j =  65 ; j <= (65 + n - 1) ; j++)
+    //     {
+    //         cout << char(i + 64) << " ";
+    //     }
+    //         cout << endl;
+    // }
+
+    // 2nd Way ---------------------------------------------------------
+
+    // for (int i = 1; i <= n; i++)
+    // {
+    //     for (char j = 'A'; j <= (char)(64 + n); j++)
+    //     {
+    //         cout << (char)(i + 64) << " ";
+    //     }
+    //     cout << endl;
+    // }
+
+    // 3rd Way ----------------------------------------------------------
+
+    for(int i = 1 ; i <= n ; i++) // i++ // i += 1 // i = i + 1
+    {
+        for (int j = 1 ; j <= n ; j++)
+        {
+            if (n >= 1 && n <= 26)
+
+            cout << (char)(i + 64) << " ";
+        }
+            cout << endl;
+    }
+
+    return 0;
+}
