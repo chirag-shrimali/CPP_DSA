b0527880ad8ef8636271985a9f3e25d407e50ddd
Chirag Shrimali <chiragshrimali0411@gmail.com>
2026-07-12 00:40:44 +0530

    Star Square Pattern

---
+++ Patterns/Star_Square.cpp
@@ -0,0 +1,50 @@
+/*
+
+    Print the Following Star Rectangle Patterns...
+
+    * * * *
+    * * * *
+    * * * *
+    * * * *
+
+*/
+
+// Boiler Plate Code...
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    // 1st Way -----------------------------------------------------------------
+
+    // cout << "* * * *" << endl;
+
+    // cout << "* * * *" << endl;
+
+    // cout << "* * * *" << endl;
+
+    // cout << "* * * *" << endl;
+
+    // 2nd Way -----------------------------------------------------------------
+
+
+    // for (int i = 1 ; i <= 4 ; i++) // i++ // i += 1 // i = i + 1
+    // {
+    //     cout << "* * * *" << endl;
+    // }
+
+    // 3rd Way -----------------------------------------------------------------
+
+    for (int i = 1 ; i <= 4 ; i++) // i++ // i += 1 // i = i + 1
+    {
+        for (int j = 1 ; j <= 4 ; j++)
+        {
+            cout << "* ";
+        }
+            cout << endl;
+    }
+
+    return 0;
+}
