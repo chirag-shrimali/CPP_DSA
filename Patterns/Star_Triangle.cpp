2e3ad0f919d41b50caae0313d9ca8cdc029df8cc
Chirag Shrimali <chiragshrimali0411@gmail.com>
2026-07-12 01:44:24 +0530

    Star Triangle Pattern -1

---
+++ Patterns/Star_Triangle.cpp
@@ -0,0 +1,34 @@
+/*
+
+    Print the Following Star Rectangle Patterns...
+
+    *
+    * *
+    * * *
+    * * * *
+
+*/
+
+// Boiler Plate Code...
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+
+    cout << "\nEnter the Value Of N : ";
+
+    cin >> n;
+
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+}
