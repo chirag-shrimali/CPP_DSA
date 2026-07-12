4177a407c671409f614cd0ccb7a6fea0a1dabb07
Chirag Shrimali <chiragshrimali0411@gmail.com>
2026-07-12 23:24:06 +0530

    Star Plus Pattern

---
+++ Patterns/Star_Plus.cpp
@@ -0,0 +1,44 @@
+/*
+
+    Print the Following Star Square Patterns...
+
+        *
+        *
+    * * * * *
+        *
+        *
+
+*/
+
+// Boiler Plate Code...
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+
+    cout << "\nEnter the Value of N : ";
+
+    cin >> n;
+
+    for (int i = 1; i <= n; i++) // i++ // i += 1 // i = i + 1
+    {
+        for (int j = 1; j <= n; j++)
+        {
+
+            if(i == (n / 2 + 1) || j == (n / 2 + 1))
+
+            cout << "* ";
+
+            else
+                cout << "  ";
+
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
