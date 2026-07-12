dbeb906853b62102a3c3ed3b0efd3a30d7604c31
Chirag Shrimali <chiragshrimali0411@gmail.com>
2026-07-12 21:50:44 +0530

    Number Triangle Pattern - 1

---
+++ Patterns/Number_Triangle.cpp
@@ -0,0 +1,34 @@
+/*
+
+    Print the Following Star Number Patterns...
+
+    1
+    1 2
+    1 2 3
+    1 2 3 4
+
+*/
+
+// Boiler Plate Code...
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+
+    cout << "\nEnter the Value Of N : ";
+
+    cin >> n;
+
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout << j << " ";
+        }
+        cout << endl;
+    }
+}
