ae0ab37263d735b73c48641bb69ea694ddba4fa8
Chirag Shrimali <chiragshrimali0411@gmail.com>
2026-07-12 22:01:09 +0530

    Alphanumeric Triangle Pattern - 1

---
+++ Patterns/Alphanumeric_Triangle.cpp
@@ -0,0 +1,40 @@
+/*
+
+    Print the Following Alphabet Triangle Patterns...
+
+    A
+    A B
+    A B C
+    A B C D
+
+*/
+
+// Boiler Plate Code...
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+
+    cout << "\nEnter the Value Of N : ";
+
+    cin >> n;
+
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {   
+            if(i % 2 == 0)
+            {
+                cout << (char)(j + 64)  << " ";
+            }
+            else{
+                cout << j << " ";
+            }
+        }
+        cout << endl;
+    }
+}
